c014b01749b63f707ac7e812da0bdd0145c9faa1
nicolofabris <nicolo.fabriss@gmail.com>
2023-07-14 13:05:52 +0200

    forgot password

--- Desktop/Udemy/React/chatty-front/chatty/src/pages/auth/login/Login.js
+++ Desktop/Udemy/React/chatty-front/chatty/src/pages/auth/login/Login.js
@@ -1,33 +1,61 @@
-import React from 'react'
-import {FaArrowRight} from 'react-icons/fa'
-import Input from '../../../components/input/Input'
-import Button from '../../../components/button/Button'
+import React from "react";
+import { FaArrowRight } from "react-icons/fa";
+import Input from "../../../components/input/Input";
+import Button from "../../../components/button/Button";
+import { Link } from "react-router-dom";
 
-const login = () => {
+const Login = () => {
   return (
     <div className="auth-inner">
-    <div className="alerts" role="alert">
+      {/* <div className="alerts" role="alert">
         Error message
-    </div>
-    <form className="auth-form">
+    </div> */}
+      <form className="auth-form">
         <div className="form-input-container">
-            <Input id="username" name="username" type="text" value='my value' labelText='Username' placeholder="Enter username" handleChange={() => {}}/>
-            <Input id="password" name="password" type="password" value='my password' labelText='Password' placeholder="Enter password" handleChange={() => {}}/>
-            <Input id="checkbox" name="checkbox" type="checkbox" value={false} handleChange={() => {}}/>
-            
-            <label className="checkmark-container" htmlFor="checkbox">
-                <input id="checkbox" type="checkbox" name="checkbox" />
-                Keep me signed in
-            </label>
+          <Input
+            id="username"
+            name="username"
+            type="text"
+            value="my value"
+            labelText="Username"
+            placeholder="Enter username"
+            handleChange={() => {}}
+          />
+          <Input
+            id="password"
+            name="password"
+            type="password"
+            value="my password"
+            labelText="Password"
+            placeholder="Enter password"
+            handleChange={() => {}}
+          />
+          <Input
+            id="checkbox"
+            name="checkbox"
+            type="checkbox"
+            value={false}
+            handleChange={() => {}}
+          />
+
+          <label className="checkmark-container" htmlFor="checkbox">
+            <input id="checkbox" type="checkbox" name="checkbox" />
+            Keep me signed in
+          </label>
         </div>
-        <Button label={'LOGIN'} className="auth-button button" disabled={true}/>
+        <Button
+          label={"SIGNIN"}
+          className="auth-button button"
+          disabled={true}
+        />
+        <Link to={'/forgot-password'}>
+          <span className="forgot-password">
+            Forgot password? <FaArrowRight className="arrow-right" />
+          </span>
+        </Link>
+      </form>
+    </div>
+  );
+};
 
-        <span className="forgot-password">
-            Forgot password? <FaArrowRight className='arrow-right'/>
-        </span>
-    </form>
-</div>
-  )
-}
-
-export default login
+export default Login;
--- Desktop/Udemy/React/chatty-front/chatty/src/routes.js
+++ Desktop/Udemy/React/chatty-front/chatty/src/routes.js
@@ -1,13 +1,18 @@
 import { lazy, Suspense } from "react";
 import { AuthTabs } from "./pages/auth";
 import { useRoutes } from "react-router-dom";
+import ForgotPassword from "./pages/auth/forgot-password/ForgotPassword";
 
 export const AppRouter = () => {
     const elements = useRoutes([
         {
             path: '/',
             element: <AuthTabs/>
-        }
+        },
+        {
+            path: '/forgot-password',
+            element: <ForgotPassword/>
+        },
     ])
 
     return elements
